d96e4ac8995a49e7964d3a2f267bb345cb46ae86
Max <1998max7362@mail.ru>
2023-07-10 23:33:45 +0300

    fsdfs

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ import time
 import asyncio
 from random import *
 from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Define a WebSocket endpoint
-@app.get("/")
-async def homepage():
-    data = json.dumps({'hello': 'world'})
-    return data
 
 
 @app.websocket("/ws_limb")
@@ -31,7 +41,6 @@ async def send_events(websocket: WebSocket):
     i = 0
     while True:
         i += 1
-
 
         data = {
             "events": [
@@ -155,3 +164,76 @@ def messageChanger(message):
 def messageLogicChanger(jsonMessage):
     jsonMessage['centerFrequency'] = 1e7
     return jsonMessage
+
+
+
+fakeDeviceResponse = {
+    "devices": [
+        {
+            "connected": True,
+            "devModel": "ESMD",
+            "SN": "111122",
+            "isRec": True,
+            "isDf": True,
+            "isScan": True,
+            "devAddress": "192.168.1.1",
+            "devMessagePort": 5555,
+            "localDataPort": 31001,
+            "dataProcAddr": "ws://192.168.1.2:30001",
+            "controlPanelAddr": "ws://192.168.1.2:30002",
+            "chartControlAddr": "ws://192.168.1.2:30003",
+            "eventsPanelAddr": "ws://192.168.1.2:30004",
+            "rawEventsAddr": "ws://192.168.1.2:30005"
+        },
+        {
+            "connected": True,
+            "devModel": "ESMD",
+            "SN": "222222",
+            "isRec": True,
+            "isDf": True,
+            "isScan": True,
+            "devAddress": "199.168.1.1",
+            "devMessagePort": 7777,
+            "localDataPort": 41001,
+            "dataProcAddr": "ws://192.168.1.2:30006",
+            "controlPanelAddr": "ws://192.168.1.2:30007",
+            "chartControlAddr": "ws://192.168.1.2:30008",
+            "eventsPanelAddr": "ws://192.168.1.2:30009",
+            "rawEventsAddr": "ws://192.168.1.2:30010"
+        }
+    ],
+    "serverName": "point_on_pentagon_roof",
+    "description": "some words here"
+}
+
+
+class Device(BaseModel):
+    connected: bool
+    devModel: str
+    SN: str
+    isRec: bool
+    isDf: bool
+    isScan: bool
+    devAddress: str
+    devMessagePort: str
+    localDataPort: str
+    dataProcAddr: str
+    controlPanelAddr: str
+    chartControlAddr: str
+    eventsPanelAddr: str
+    rawEventsAddr: str
+
+class DeviceResponse(BaseModel):
+    devices: List[Device]
+    serverName: str
+    description: str
+
+
+@app.get("/devices", response_model=DeviceResponse)
+async def answerDevices():
+    return fakeDeviceResponse
+
+@app.get("/")
+async def homepage():
+    data = json.dumps({'hello': 'world'})
+    return data
